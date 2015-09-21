2566d6f390a6c9d23bf7262fb9ff3209cd7dbd82
Fynn Feldpausch <fynn.feldpausch@mindsmash.com>
2015-09-21 09:11:08 +0200

    added missing Favico functions

--- dist/mindsmash-angular-favico.js
+++ dist/mindsmash-angular-favico.js
@@ -2,14 +2,23 @@
     'use strict';
     
     function FavIco(badgeOptions) {
-        var favico = new Favico(badgeOptions);
+        var favico = badgeOptions ? new Favico(badgeOptions) : new Favico();
         
-        this.badge = function(num) {
-            favico.badge(num);
+        this.badge = function() {
+            return favico.badge.apply(this, arguments);
+        };
+        this.image = function() {
+            return favico.image.apply(this, arguments);
+        };
+        this.video = function() {
+            return favico.video.apply(this, arguments);
+        };
+        this.webcam = function() {
+            return favico.webcam.apply(this, arguments);
         };
         
         this.reset = function() {
-            favico.reset();
+            return favico.reset.apply(this, arguments);
         };
     }
     
@@ -30,8 +39,8 @@
             'dataUrl'    : false
         };
         
-        this.setOptions = function(options) {
-            angular.extend(badgeOptions, options);
+        this.setOptions = function(options, useDefaults) {
+            badgeOptions = useDefaults ? angular.extend(badgeOptions, options) : options;
         };
         
         this.$get = function() {
